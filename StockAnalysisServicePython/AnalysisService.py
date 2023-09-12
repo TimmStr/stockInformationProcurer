197f253d0465f5e1a31e2f3084085dbaa60ec2a8
Achilleus <timm.straub@gmx.de>
2023-09-12 22:58:12 +0200

    Python Docker Container is running on Python 3.6. First plots are being saved in docker container.

--- StockAnalysisServicePython/AnalysisService.py
+++ StockAnalysisServicePython/AnalysisService.py
@@ -1,4 +1,7 @@
-from matplotlib import pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
 def start_analysis_for_symbol(collection, symbol='TSLA'):
     entries = collection.find({"Symbol": symbol})
     print(entries)
@@ -14,7 +17,15 @@ def start_analysis_for_symbol(collection, symbol='TSLA'):
     draw_graph(date, close_val, high_val, volume)
     return {'Avg:': (sum(open_val) / len(open_val)), 'Max:': max(high_val), 'Min': min(low_val)}
 
+
 def draw_graph(date, close_val, high_val, volume):
-    plt.plot(date, close_val, color='b', label='Close')
-    plt.plot(date, high_val, color='g', label='High')
-    plt.show()
+    date_transformed = [i for i, da in enumerate(date)]
+
+    df = pd.DataFrame(list(zip(date_transformed, close_val)))
+    sns.lineplot(
+        data=df,
+        # x="Date", y="Close"
+    ).figure.savefig("test_1.png")
+    # plt.plot(date, close_val, color='b', label='Close')
+    # plt.plot(date, high_val, color='g', label='High')
+    # plt.savefig('test_1.png')
