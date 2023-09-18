1e7d93c454863f9738fdddb7b5fe5c73b36cb482
Achilleus <timm.straub@gmx.de>
2023-09-18 21:30:56 +0200

    Added Swagger to Stock Analysis Service

--- StockAnalysisService/app.py
+++ StockAnalysisService/app.py
@@ -2,7 +2,8 @@ import os
 
 import requests
 from flask import Flask, request, send_file
-from flask_restful import Api
+from flask_restx import Api, Resource, fields
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from AnalysisService import start_analysis_for_ticker
 from Path.paths import *
@@ -12,56 +13,78 @@ app = Flask(__name__)
 api = Api(app)
 app.config['JSON_SORT_KEYS'] = True
 
+# URL for Swagger UI
+SWAGGER_URL = '/api/docs'
+# Location of the swagger.json
+API_URL = 'http://localhost:9030/swagger.json'
 
-stock_scraping_service = STOCK_WEB_SCRAPING_SERVICE
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Stock Web Scraping Service"
+    }
+)
 
+app.register_blueprint(swaggerui_blueprint)
 
-@app.route('/mail', methods=['GET'])
-def test_mail():
-    URL = MAIL_SERVICE + 'get'
-    response = requests.get(URL)
-    return response.content
-
-
-@app.route('/startAnalysis', methods=['GET'])
-def start_analysis():
-    request_values = request.values
-
-    if request_values.get('ticker') is not None:
-        try:
-            stock_response = requests.get(STOCK_WEB_SCRAPING_SERVICE_GET_STOCK_FROM_TICKER,
-                                          params=request_values.to_dict())
-            stocks = stock_response.content
-
-            # ToDo StockWebScrapingService muss umgebaut werden. Insbesonder gspread_scraper (jsonify)??? oder gleich als worksheet?
-            print(stocks)
-            print(type(stocks))
-            print(type(stocks.keys))
-            print(type(stocks.values))
-            kpi_response = requests.get(STOCK_WEB_SCRAPING_SERVICE_GET_KPIS_FROM_TICKER,
-                                        params=request_values.to_dict())
-            kpis = kpi_response.content
-            print(kpis)
-            start_analysis_for_ticker(stocks["Ticker"], stocks["Stocks"])
-            return stock_response
-        except:
-            return ERROR_OCCURED
-    else:
-        return PASS_A_TICKERSYMBOL
+ticker_parameter = api.model('Parameter', {
+    'ticker': fields.String(required=True, description='Das Tickersymbol z.B. NASDAQ:AAPL')
+})
 
 
-@app.route('/get_graphs', methods=['GET'])
-def get_graphs():
-    symbol = request.values.get('symbol')
-    date = request.values.get('date')
-    file_name = GRAPHS + symbol + date + '.png'
-    if request.values.get('period') is not None:
-        period = request.values.get('period')
-    if os.path.exists(file_name):
-        return send_file(file_name)
-    else:
-        # ToDo Get Data from Database and create graphs
-        return {"Muss noch eingefügt werden": True}
+@api.route('/mail')
+class GetMail(Resource):
+    def get(self):
+        URL = MAIL_SERVICE + 'get'
+        response = requests.get(URL)
+        return response.content
+
+
+@api.route('/startAnalysis')
+class StartAnalysis(Resource):
+    @api.expect(ticker_parameter)
+    def get(self):
+        request_values = request.values
+
+        if request_values.get('ticker') is not None:
+            try:
+                stock_response = requests.get(STOCK_WEB_SCRAPING_SERVICE_GET_STOCK_FROM_TICKER,
+                                              params=request_values.to_dict())
+                stocks = stock_response.content
+
+                # ToDo StockWebScrapingService muss umgebaut werden. Insbesonder gspread_scraper (jsonify)??? oder gleich als worksheet?
+                print(stocks)
+                print(type(stocks))
+                print(type(stocks.keys))
+                print(type(stocks.values))
+                kpi_response = requests.get(STOCK_WEB_SCRAPING_SERVICE_GET_KPIS_FROM_TICKER,
+                                            params=request_values.to_dict())
+                kpis = kpi_response.content
+                print(kpis)
+                start_analysis_for_ticker(stocks["Ticker"], stocks["Stocks"])
+                return stock_response
+            except:
+                return ERROR_OCCURED
+        else:
+            return PASS_A_TICKERSYMBOL
+
+
+@api.route('/get_graphs')
+class GetGraphs(Resource):
+    @api.expect(ticker_parameter)
+    def get(self):
+        ticker = request.values.get('ticker')
+        date = request.values.get('date')
+        file_name = GRAPHS + ticker + date + '.png'
+        if request.values.get('period') is not None:
+            period = request.values.get('period')
+        if os.path.exists(file_name):
+            return send_file(file_name)
+        else:
+            # ToDo Get Data from Database and create graphs
+            return {"Muss noch eingefügt werden": True}
 
 
 if __name__ == '__main__':
